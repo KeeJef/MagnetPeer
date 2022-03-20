0eec4eee1d76325d5c3208408cb419e8f2f0b889
Kee Jefferys <kee@loki.network>
2022-03-20 20:58:33 +1100

    Basic design

---
+++ server.js
@@ -0,0 +1,46 @@
+const webtorrentHealth = require('webtorrent-health')
+var express = require('express');
+var app = express();
+var base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+
+
+// Get Request for single b64 encoded magnet
+app.get('/m/single', async (req, res) =>{
+    if (req.query.magnet) {
+        if (base64regex.test(req.query.magnet)) {
+            
+            decodedMagnet = decodeb64(req.query.magnet)
+            torrentprom = await checkTorrentPromise(decodedMagnet)
+
+            var response = new Object()
+            response.magnetLink = decodedMagnet
+            response.seeds = torrentprom.seeds
+            response.leachers = torrentprom.peers
+
+            res.end(JSON.stringify(response));
+            
+        }
+        else{
+            res.end("Magnet not B64 encoded");
+        }
+
+    }
+    else{
+        res.end("No magnet provided");
+    }
+    
+})
+//Gets promise from webtorrenthealth with information about seeds and peers
+function checkTorrentPromise(magnetURL) {
+    return webtorrentHealth(magnetURL)
+}
+
+function decodeb64(data){
+    data = Buffer.from(data, 'base64').toString('ascii');
+    return data
+}
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
